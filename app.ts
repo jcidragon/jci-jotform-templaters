ebc824c4fb3cd05c0c6c64c501b0540b69030f6f
Alex Lau <alex@tecky.io>
2018-11-29 01:26:52 +0800

    Add default template

--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
 import * as multer from 'multer';
+import { readFile } from "fs";
+import { promisify } from "util";
 import axios from 'axios';
 import { convert } from './convert';
+
+const readFilePromise = promisify(readFile);
 
 dotenv.config();
 
@@ -27,6 +31,18 @@ app.post('/generate', upload.single('template'), async (req, res) => {
   try {
     const submissionsResponse = await axios.get(`http://api.jotform.com/form/83159166506461/submissions?apiKey=${process.env.API_KEY}`);
     const buffer = convert(submissionsResponse.data, req.file.buffer);
+
+    res.type('docx');
+    res.send(buffer);
+  } catch (e) {
+    res.send(e.message);
+  }
+})
+
+app.post('/generate-default', async (req, res) => {
+  try {
+    const submissionsResponse = await axios.get(`http://api.jotform.com/form/83159166506461/submissions?apiKey=${process.env.API_KEY}`);
+    const buffer = convert(submissionsResponse.data, await readFilePromise(__dirname + '/template-report.docx'));
 
     res.type('docx');
     res.send(buffer);
